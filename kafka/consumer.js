281182a1dcba673cf488be3b54a09b1d7fd2407f
Nikhil Nar <nik.nar123@gmail.com>
2019-05-02 00:29:30 -0400

    Implemented consumer group

--- kafka/consumer.js
+++ kafka/consumer.js
@@ -1,40 +1,46 @@
 var kafka = require('kafka-node');
 //var HighLevelConsumer = kafka.HighLevelConsumer;
-var Consumer = kafka.Consumer;
+var ConsumerGroup = kafka.ConsumerGroup;
 var Client = kafka.Client;
 const client = new kafka.KafkaClient({
   kafkaHost: '192.168.99.100:9092'
 });
-var topics = [{
-  topic: 'movie_ratings',
-  partition:0
-},
-{
-  topic: 'movie_ratings',
-  partition:1
-},
-{
-  topic: 'movie_ratings',
-  partition:2
-},
-{
-  topic: 'movie_users_mapping'
-}];
+var topics = ['movie_users_mapping','movie_ratings'];
+var options = {
+  kafkaHost: '192.168.99.100:9092', // connect directly to kafka broker (instantiates a KafkaClient)
+  batch: undefined, // put client batch settings if you need them
+  ssl: true, // optional (defaults to false) or tls options hash
+  groupId: 'ExampleTestGroup',
+  sessionTimeout: 15000,
+  // An array of partition assignment protocols ordered by preference.
+  // 'roundrobin' or 'range' string for built ins (see below to pass in custom assignment protocol)
+  protocol: ['roundrobin'],
+  encoding: 'utf8', // default is utf8, use 'buffer' for binary data
+
+  // Offsets to use for new groups other options could be 'earliest' or 'none' (none will emit an error if no offsets were saved)
+  // equivalent to Java client's auto.offset.reset
+  fromOffset: 'latest', // default
+  commitOffsetsOnFirstJoin: true, // on the very first time this consumer group subscribes to a topic, record the offset returned in fromOffset (latest/earliest)
+  // how to recover from OutOfRangeOffset error (where save offset is past server retention) accepts same value as fromOffset
+  outOfRangeOffset: 'earliest', // default
+  // Callback to allow consumers with autoCommit false a chance to commit before a rebalance finishes
+  // isAlreadyMember will be false on the first connection, and true on rebalances triggered after that
+  onRebalance: (isAlreadyMember, callback) => { callback(); } // or null
+};
+
+var consumerGroup = new ConsumerGroup(options, topics);
 
 
-var consumer = new Consumer(client, topics);
-
-
-consumer.on('message', function(message) {
+consumerGroup.on('message', function(message) {
   console.log(message);
 });
 
-consumer.on('error', function(err) {
+consumerGroup.on('error', function(err) {
   console.log('error', err);
 });
 
 process.on('SIGINT', function() {
-  consumer.close(true, function() {
+  consumerGroup.close(true, function() {
     process.exit();
   });
 });
